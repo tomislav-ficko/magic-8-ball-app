ad6eaf3d56952efca281cb9c4ff41c6259138f5a
Tomislav Ficko <ficko.tomislav@gmail.com>
2020-03-28 12:58:50 +0100

    Add elements to main storyboard

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,8 +12,16 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    @IBOutlet weak var askButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        askButton.layer.cornerRadius = 5
+        
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
-
