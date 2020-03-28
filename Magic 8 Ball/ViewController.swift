77770c1236e71515a5a185ae71102852e5aa7d48
Tomislav Ficko <ficko.tomislav@gmail.com>
2020-03-28 11:35:35 +0100

    Initial commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -2,9 +2,10 @@
 //  ViewController.swift
 //  Magic 8 Ball
 //
-//  Created by Angela Yu on 14/06/2019.
+//  Originally created by Angela Yu on 14/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
+//  Created by Tomislav Ficko on 28/03/2020.
 
 import UIKit
 
