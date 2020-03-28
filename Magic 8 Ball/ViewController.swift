3e6408bf844708e02b2ac067a3880dbeab25de12
Tomislav Ficko <ficko.tomislav@gmail.com>
2020-03-28 13:18:09 +0100

    Add randomisation functionality

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -2,9 +2,6 @@
 //  ViewController.swift
 //  Magic 8 Ball
 //
-//  Originally created by Angela Yu on 14/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 //  Created by Tomislav Ficko on 28/03/2020.
 
 import UIKit
@@ -12,16 +9,21 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var askButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        askButton.layer.cornerRadius = 5
-        
+        askButton.layer.cornerRadius = 10
+    
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
-        
+        imageView.image = ballArray[randomNumber()]
+    }
+    
+    func randomNumber() -> Int {
+        return Int.random(in: 0...4)
     }
     
 }
